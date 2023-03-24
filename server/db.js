a63b0f6105189edf2c41d052abeb07808f676d36
“arifcseiu0809git” <arifcseiu0809@gmail.com>
2023-03-24 23:20:06 +0600

    initial commit

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+// const dbUri = 'mongodb+srv://admin:1234@cluster0.akzacmt.mongodb.net/employee_db?retryWrites=true&w=majority'
+const dbUri = 'mongodb://localhost:27017/CrudDB?retryWrites=true&w=majority'
+
+mongoose.set('strictQuery', false)
+
+module.exports = () => {
+    return mongoose.connect(dbUri,
+        { useNewUrlParser: true, useUnifiedTopology: true })
+}
